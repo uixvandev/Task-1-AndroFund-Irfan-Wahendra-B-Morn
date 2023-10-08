e82e7a02634e908b5e466f200124eac54dca9bf7
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-10-09 01:21:17 +0700

    first

---
+++ app/src/main/java/com/infinitelearning/tugas/SignIn.kt
@@ -0,0 +1,11 @@
+package com.infinitelearning.tugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
