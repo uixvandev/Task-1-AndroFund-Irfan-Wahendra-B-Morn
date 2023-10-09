997dcdeb4f41902469cf3d768673818cd3fa3668
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-10-09 21:01:00 +0700

    Add link punya akun

--- app/src/main/java/com/infinitelearning/tugas/SignIn.kt
+++ app/src/main/java/com/infinitelearning/tugas/SignIn.kt
@@ -1,11 +1,32 @@
 package com.infinitelearning.tugas
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.infinitelearning.tugas.R.*
+import com.infinitelearning.tugas.R.id.*
 
-class SignIn : AppCompatActivity() {
+class SignIn : AppCompatActivity(), View.OnClickListener {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(layout.activity_sign_in)
+
+        val btnSignIn: Button = findViewById(btn_signin)
+        btnSignIn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            btn_signin -> {
+                val intent = Intent(this@SignIn, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
     }
 }
--- app/src/main/java/com/infinitelearning/tugas/SignUpActivity.kt
+++ app/src/main/java/com/infinitelearning/tugas/SignUpActivity.kt
@@ -1,11 +1,40 @@
 package com.infinitelearning.tugas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btnRegister: Button = findViewById(R.id.btn_regist)
+        btnRegister.setOnClickListener(this)
+
+        val signInTextView = findViewById<TextView>(R.id.link_signIn)
+        signInTextView.setOnClickListener {
+
+            val myIntent = Intent(this, SignIn::class.java)
+            startActivity(myIntent)
+
+            signInTextView.movementMethod = LinkMovementMethod.getInstance()
+
+        }
+
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_regist-> {
+                val intent = Intent(this@SignUpActivity, SignIn::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
