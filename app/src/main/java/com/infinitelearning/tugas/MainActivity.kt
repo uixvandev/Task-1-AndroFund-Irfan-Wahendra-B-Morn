0dad4a6ca7d7aed010216b33772aa53a8f51b807
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-10-10 00:01:28 +0700

    Add implicit button

--- app/src/main/java/com/infinitelearning/tugas/MainActivity.kt
+++ app/src/main/java/com/infinitelearning/tugas/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.infinitelearning.tugas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnImplicit : Button = findViewById(R.id.btn_implicit)
+        btnImplicit.setOnClickListener(this)
+    }
 
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_implicit -> {
+                val message = "Halo Test"
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+                startActivity(intent)
+            }
+        }
     }
 }
